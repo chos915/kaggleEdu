d81e217dcb0ff54129c57af8b5b8f326a358e782
chos915 <chos915@hanmail.net>
2023-03-30 12:41:38 +0900

    updated

---
+++ temp.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+import numpy as np
+import seaborn as sns
+import matplotlib as mpl
+import sklearn
+
+print(pd.__version__)
+print(np.__version__)
+print(sns.__version__)
+print(mpl.__version__)
+print(sklearn.__version__)
